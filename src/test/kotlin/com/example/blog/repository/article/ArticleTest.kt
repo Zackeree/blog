3c3263f03327a8a063b6b147f211a541186d282f
Zackeree <zhenry@ycp.edu>
2019-03-24 16:51:00 -0400

    Clean up duplicate code in UserTest

--- src/test/kotlin/com/example/blog/repository/article/ArticleTest.kt
+++ src/test/kotlin/com/example/blog/repository/article/ArticleTest.kt
@@ -1,2 +1,18 @@
 package com.example.blog.repository.article
 
+import com.example.blog.repository.article.IArticleRepository
+import com.example.blog.repository.article.Article
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+
+
+@DataJpaTest
+internal class ArticleTest @Autowired constructor(
+        val entityManager: TestEntityManager,
+        val articleRepository: IArticleRepository) {
+
+
+}
--- src/test/kotlin/com/example/blog/repository/user/UserTest.kt
+++ src/test/kotlin/com/example/blog/repository/user/UserTest.kt
@@ -1,7 +1,7 @@
 package com.example.blog.repository.user
 
-import org.aspectj.lang.annotation.Before
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
@@ -12,18 +12,20 @@ internal class UserTest @Autowired constructor(
                val entityManager: TestEntityManager,
                val userRepository: IUserRepository) {
 
-  //  @Before
-    //fun setUp() {
+    // Initial Fields
+    private lateinit var zack: User
 
-    //}
-
+    // Run this before each @Test
+    @BeforeEach
+    fun setup () {
+        zack = User("Zack", "Zackeree", "Henry")
+        entityManager.persist(zack)
+        entityManager.flush()
+    }
 
     @Test
     fun `Test findByUsername and return User`() {
         println(">> testing findByUsername")
-        val zack = User("Zack", "Zackeree", "Henry")
-        entityManager.persist(zack)
-        entityManager.flush()
         val user = userRepository.findByUsername("Zack")
         assertThat(user).isEqualTo(zack);
     }
@@ -31,9 +33,6 @@ internal class UserTest @Autowired constructor(
     @Test
     fun `Test findByFirstName and return User`() {
         println(">> testing findByFirstName")
-        val zack = User("Zack", "Zackeree", "Henry")
-        entityManager.persist(zack)
-        entityManager.flush()
         val user = userRepository.findByFirstName("Zackeree")
         assertThat(user).isEqualTo(zack);
     }
@@ -41,9 +40,6 @@ internal class UserTest @Autowired constructor(
     @Test
     fun `Test findByLastName and return User`() {
         println(">> testing findByLastName")
-        val zack = User("Zack", "Zackeree", "Henry")
-        entityManager.persist(zack)
-        entityManager.flush()
         val user = userRepository.findByLastName("Henry")
         assertThat(user).isEqualTo(zack);
     }
