4ce8dab3099d87329361f4ad6d1afd44f1efa00b
Zackeree <zhenry@ycp.edu>
2019-03-24 18:28:43 -0400

    Implement content viewing on main page

--- src/main/kotlin/com/example/blog/controller/HtmlController.kt
+++ src/main/kotlin/com/example/blog/controller/HtmlController.kt
@@ -1,18 +1,52 @@
 package com.example.blog.controller
 
+import com.example.blog.format
+import com.example.blog.repository.article.Article
+import com.example.blog.repository.article.IArticleRepository
+import com.example.blog.repository.user.User
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
-class HtmlController {
+class HtmlController (private val repository: IArticleRepository) {
 
     @GetMapping("/")
     fun blog(model: Model): String {
         // org.springframework.ui.set allows us to declare model in this fashion
         // as opposed to model.addAttribute("title", "Blog")
         model["title"] = "Blog"
+        model["articles"] = repository.findAllByOrderByDateDesc().map { it.render() }
         return "blog"
     }
+
+    @GetMapping("/article/{slug}")
+    fun article(@PathVariable slug: String, model: Model): String {
+        val article = repository
+                .findBySlug(slug)
+                ?.render()
+                ?: throw IllegalArgumentException("Wrong article slug provided")
+        model["title"] = article.title
+        model["article"] = article
+        return "article"
+    }
+
+    fun Article.render() = RenderedArticle(
+            slug,
+            title,
+            headline,
+            content,
+            author,
+            date.format()
+    )
+
+    data class RenderedArticle(
+            val slug: String,
+            val title: String,
+            val headline: String,
+            val content: String,
+            val author: User,
+            val date: String)
 }
