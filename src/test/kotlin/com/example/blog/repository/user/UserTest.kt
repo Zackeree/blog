d2acf13ec3aac29bec65e865b8f7425942c65f35
Zackeree <zhenry@ycp.edu>
2019-03-24 16:00:16 -0400

    Add additional tests to UserTest
    Searching by user First and Last name is now possible

--- src/test/kotlin/com/example/blog/repository/user/UserTest.kt
+++ src/test/kotlin/com/example/blog/repository/user/UserTest.kt
@@ -1,6 +1,5 @@
 package com.example.blog.repository.user
 
-import com.example.blog.repository.user.IUserRepository
 import org.aspectj.lang.annotation.Before
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -9,9 +8,15 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 
 @DataJpaTest
-class UserTest @Autowired constructor(
+internal class UserTest @Autowired constructor(
                val entityManager: TestEntityManager,
                val userRepository: IUserRepository) {
+
+  //  @Before
+    //fun setUp() {
+
+    //}
+
 
     @Test
     fun `Test findByUsername and return User`() {
@@ -20,6 +25,26 @@ class UserTest @Autowired constructor(
         entityManager.persist(zack)
         entityManager.flush()
         val user = userRepository.findByUsername("Zack")
+        assertThat(user).isEqualTo(zack);
+    }
+
+    @Test
+    fun `Test findByFirstName and return User`() {
+        println(">> testing findByFirstName")
+        val zack = User("Zack", "Zackeree", "Henry")
+        entityManager.persist(zack)
+        entityManager.flush()
+        val user = userRepository.findByFirstName("Zackeree")
+        assertThat(user).isEqualTo(zack);
+    }
+
+    @Test
+    fun `Test findByLastName and return User`() {
+        println(">> testing findByLastName")
+        val zack = User("Zack", "Zackeree", "Henry")
+        entityManager.persist(zack)
+        entityManager.flush()
+        val user = userRepository.findByLastName("Henry")
         assertThat(user).isEqualTo(zack);
     }
 }
